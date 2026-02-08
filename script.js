6238acc846e0bb5fc30d5daa35e6d3129b9f13b1
Boobalansenthil <boobalan8116@gmail.com>
2026-02-08 17:23:11 +0530

    Second

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input; 
+} 
+function clearDisplay(){
+    display.value = "";
+}
+function calculateResult(){
+    try {   
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+function clearLastElement(){
+    display.value = display.value.slice(0, -1);
+}
